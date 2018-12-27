b753f7f4d715ae362856289da2c9f5fbfcdb811c
James Corbould <james.bron@hotmail.com>
2018-12-27 22:17:58 +1300

    Added logic to call cosmos db.  Added route.

--- FunctionAppApi/books.cs
+++ FunctionAppApi/books.cs
@@ -1,26 +1,58 @@
-using System.Linq;
+using System;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Configuration;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
 
 namespace FunctionApp
 {
     public static class books
     {
+        private static readonly string DatabaseId = ConfigurationManager.AppSettings["database"];
+        private static readonly string CollectionId = ConfigurationManager.AppSettings["collection"];
+        private static DocumentClient client;
+
         [FunctionName("books")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route="books/{id?}")]HttpRequestMessage req, string id, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
 
-            // Get request body
-            string data = await req.Content.ReadAsStringAsync();
-            var json = JsonConvert.DeserializeObject(data);
-
-            return req.CreateResponse(HttpStatusCode.Created, json);
+            client = new DocumentClient(new Uri(ConfigurationManager.AppSettings["endpoint"]), ConfigurationManager.AppSettings["authKey"]);
+            
+            switch (req.Method.ToString().ToLower())
+            {
+                case "post":
+                    string data = await req.Content.ReadAsStringAsync();
+                    var json = JsonConvert.DeserializeObject(data);
+                    return req.CreateResponse(HttpStatusCode.Created, json);
+                case "get":
+                    try
+                    {
+                        Document document =
+                            await client.ReadDocumentAsync(UriFactory.CreateDocumentUri(DatabaseId, CollectionId, id));
+                        return req.CreateResponse(HttpStatusCode.OK, document);
+                    }
+                    catch (DocumentClientException e)
+                    {
+                        if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
+                        {
+                            return null;
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                default:
+                    return req.CreateResponse(HttpStatusCode.InternalServerError);
+            }
         }
     }
 }
